2f60bc7944399c0be4115e9cb7d40970cbd5f775
Lucas Bezerra <lucaslimavzt@gmail.com>
2024-02-20 23:02:27 -0300

    basic project startup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # formating-csv-api
-teste
+
+Esta api tem o intuito de fornecer os recursos para manusear csv files a fim de se obter um resultado final personalizado.
---
+++ src/main/java/com/solutions/formatingcsvapi/FormatingCsvApiApplication.java
@@ -0,0 +1,13 @@
+package com.solutions.formatingcsvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormatingCsvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormatingCsvApiApplication.class, args);
+	}
+
+}
